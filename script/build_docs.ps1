1a4cdf431e2fd568838c2ef50e98ce61d4dd7a18
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2021-10-29 08:20:17 +0900

    windows-rsの更新とワークスペースの整理

--- script/build_docs.ps1
+++ script/build_docs.ps1
@@ -1,11 +1,7 @@
 $prev_dir = Convert-Path .
 $dir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-Set-Location $dir/../mltg/book
-mdbook build -d ../../docs
-
-Set-Location $dir/..
-cargo doc --no-deps -p mltg
-Copy-Item -Recurse target/doc docs/api
+Set-Location $dir/../book
+mdbook build -d ../docs
 
 Set-Location $prev_dir
